48d00651c9647980acfc55fffb3b3d9fecd2c43f
gafaradetunji <gafaradetunji471@gmail.com>
2026-05-24 08:56:48 +0100

    updated the cdn link

--- frontend/src/app/header.tsx
+++ frontend/src/app/header.tsx
@@ -7,12 +7,12 @@ export const FooterScript = () => {
 
     return (
         <Script
-          src="https://cdn.jsdelivr.net/gh/3gensolution/guideai-scanner@cdn-v0.1.6/cdn/guideai.js"
+          src="https://cdn.3guideai.com/sdk/guideai.js"
           strategy="afterInteractive"
           data-site-id="39bb4c66-9342-465a-95bb-02ee66ad4688"
           data-token="pk_live_LRuDCmTwv0HYf1Rt73e2Ocj_L1oH6yAI2UgMara53ww"
           data-api-url="https://api.3guideai.com"
-          data-cdn-url="https://cdn.jsdelivr.net/gh/3gensolution/guideai-scanner@cdn-v0.1.5/cdn/guideai.js"
+          data-cdn-url="https://cdn.3guideai.com/sdk/guideai.js"
           data-track-all="true"
           data-behavioral-triggers="true"
         />
