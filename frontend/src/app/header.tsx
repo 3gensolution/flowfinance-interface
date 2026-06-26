d15b95750a30fca90c21b9ce248f2faec598594b
gafaradetunji <gafaradetunji471@gmail.com>
2026-06-26 17:22:37 +0100

    added tracking for Awinfi

--- frontend/src/app/header.tsx
+++ frontend/src/app/header.tsx
@@ -1,22 +1,32 @@
 import Script from "next/script";
 
 export const FooterScript = () => {
-    // const trackAll = process.env.NEXT_PUBLIC_GUIDEAI_TRACK_ALL ?? 'false';
-    // const behavioralTriggers = process.env.NEXT_PUBLIC_GUIDEAI_BEHAVIORAL_TRIGGERS ?? 'false';
-    // const apiUrl = process.env.NEXT_PUBLIC_GUIDEAI_API_URL ?? 'https://api.3guideai.com';
+  // const trackAll = process.env.NEXT_PUBLIC_GUIDEAI_TRACK_ALL ?? 'false';
+  // const behavioralTriggers = process.env.NEXT_PUBLIC_GUIDEAI_BEHAVIORAL_TRIGGERS ?? 'false';
+  // const apiUrl = process.env.NEXT_PUBLIC_GUIDEAI_API_URL ?? 'https://api.3guideai.com';
 
-    return (
-        <Script
-          src="https://cdn.3guideai.com/sdk/guideai.js"
-          strategy="afterInteractive"
-          data-site-id="39bb4c66-9342-465a-95bb-02ee66ad4688"
-          data-token="pk_live_LRuDCmTwv0HYf1Rt73e2Ocj_L1oH6yAI2UgMara53ww"
-          data-api-url="https://api.3guideai.com"
-          data-cdn-url="https://cdn.3guideai.com/sdk/guideai.js"
-          data-track-all="true"
-          data-behavioral-triggers="true"
-        />
-    )
+  return (
+    <>
+      <Script
+        src="https://cdn.3guideai.com/sdk/guideai-tracking.js"
+        data-site-id="39bb4c66-9342-465a-95bb-02ee66ad4688"
+        data-token="pk_live_LRuDCmTwv0HYf1Rt73e2Ocj_L1oH6yAI2UgMara53ww"
+        data-track-all="true"
+      />
+      <Script
+        src="https://cdn.3guideai.com/sdk/guideai.js"
+        strategy="afterInteractive"
+        data-site-id="39bb4c66-9342-465a-95bb-02ee66ad4688"
+        data-token="pk_live_LRuDCmTwv0HYf1Rt73e2Ocj_L1oH6yAI2UgMara53ww"
+        data-bubble-label="Awinfi"
+        // data-api-url="https://api.3guideai.com"
+        // data-cdn-url="https://cdn.3guideai.com/sdk/guideai.js"
+        data-track-all="true"
+        data-behavioral-triggers="true"
+
+      />
+    </>
+  )
 };
 
 // data-api-url="http://localhost:8000"
